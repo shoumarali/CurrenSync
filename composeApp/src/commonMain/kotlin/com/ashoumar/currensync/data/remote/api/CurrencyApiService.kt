444f516e6ec3bdc7420becac6ae56131009a67be
shoumarali <alialakbarshoumar610@gmail.com>
2025-04-09 22:39:17 +0300

    feat: re-add CurrencyApiService.kt to track API calls

--- composeApp/src/commonMain/kotlin/com/ashoumar/currensync/data/remote/api/CurrencyApiService.kt
+++ composeApp/src/commonMain/kotlin/com/ashoumar/currensync/data/remote/api/CurrencyApiService.kt
@@ -1,8 +1,56 @@
 package com.ashoumar.currensync.data.remote.api
 
-class CurrencyApiService {
+import android.util.Log
+import com.ashoumar.currensync.domain.CurrencyApiService
+import com.ashoumar.currensync.domain.model.ApiResponse
+import com.ashoumar.currensync.domain.model.Currency
+import com.ashoumar.currensync.domain.model.RequestState
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.plugins.DefaultRequest
+import io.ktor.client.plugins.HttpTimeout
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.request.get
+import io.ktor.client.request.headers
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
+
+class CurrencyApiServiceImpl : CurrencyApiService{
+
     companion object{
         const val ENDPOINT= "https://api.currencyapi.com/v3/latest"
-        const val API_KEY = "Your api key goes here"
+        const val API_KEY = "Enter your api key here"
+    }
+
+    private val httpClient = HttpClient{
+        install(ContentNegotiation){
+            json(Json {
+                prettyPrint = true
+                isLenient = true
+                ignoreUnknownKeys = true
+            })
+        }
+        install(HttpTimeout){
+            requestTimeoutMillis = 15000
+        }
+        install(DefaultRequest){
+            headers{
+                append("apikey", API_KEY)
+            }
+        }
+    }
+
+    override suspend fun getLatestExchangeRates(): RequestState<List<Currency>> {
+        return try {
+            val response = httpClient.get(ENDPOINT)
+            if(response.status.value == 200){
+                val apiResponse = Json.decodeFromString<ApiResponse>(response.body())
+                RequestState.Success(data = apiResponse.data.values.toList())
+            }else {
+                RequestState.Error(message = "HTTP error code: ${response.status}")
+            }
+        }catch (e: Exception){
+            RequestState.Error(message = e.message.toString())
+        }
     }
 }
